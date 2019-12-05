fe4eaea64558cbc09598fb63cf40733b308739b3
Student <example@example.com>
2019-12-04 19:17:28 -0800

    ad images and script

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,17 @@ $(document).ready(function() {
   $("form#test").submit(function(event) {
   var weapon = parseInt($("select#weapon").val());
   var color = parseInt($("select#color").val());
+  var rebels = parseInt($("selector#rebels").val());
   var score = weapon + color;
 
   if (score > 5) {
     var character = "han solo"
+    $("#han").show();
+    $("#luke").hide();
   } else if(score <= 4) {
     var character = "luke skywalker"
+    $("#luke").show();
+    $("#han").hide();
   }
   $("#character").text(character);
   $("#results").show();
